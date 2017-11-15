e91c783ea164892f80d3de56b1e35ae5fe06ace2
nom1lk <steve.condylios@gmail.com>
2017-11-16 00:36:30 +1100

    Removed letters example since strange error with letter c. Commented remaining code

--- Cbinding vectors of varying lengths into a dataframe.R
+++ Cbinding vectors of varying lengths into a dataframe.R
@@ -1,60 +1,39 @@
+
+# Make some vectors of random length
 x <- 1
 for (i in 1:5) {
   assign(paste("v", i, sep=""),abs(seq(1,ceiling(rnorm(1,10,4)),1)))
 }
 
+v1
+v2
+v3
+v4
+v5
+
+# Put vector names into an object
 group <- c()
 for (i in 1:5) {
   group[i] <- paste("v",i,sep="")
 }
 
+# Get the length of the longest vector
 g <- c()
 for (i in 1:length(group)) {
   g[i] <- length(get(group[i]))
 }
-
 max(g)
 
+# Assign NAs to the elements of each vector until all vectors are as long as the longest vector
 for (i in 1:length(group)) {
   assign(group[i], c(get(group[i]), rep(NA, max(g) - length(get(group[i])))))
 }
 
+# Bind vectors togehter and convert into dataframe
 abcde <- cbind(sapply(group[1:5], get))
 abcde <- as.data.frame(abcde)
 abcde
 
 
-
-
-
-# cbind vectors of variable lengths
-
-a <- seq(1, 10, 1)
-b <- seq(1, 15, 1)
-c <- seq(1, 6, 1)
-d <- seq(1, 12, 1)
-e <- seq(1, 11, 1)
-
-group <- letters[1:5]
-g <- c()
-
-for (i in 1:length(group)) {
-  
-  g[i] <- length(get(group[i]))
-  
-}
-
-max(g)
-
-for (i in 1:length(group)) {
-  
-  assign(group[i], c(get(group[i]), rep(NA, max(g) - length(get(group[i])))))
-  
-}
-
-abcde <- cbind(get(letters[1:5]))
-abcde
-
-c(group[i], c(1,2,3))
 
 
